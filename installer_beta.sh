f805db275939fc65de91dc20385efb4b350b7aba
Frank E <131370932+byfranke@users.noreply.github.com>
2025-01-28 16:58:24 -0300

    Update installer_beta.sh

--- installer_beta.sh
+++ installer_beta.sh
@@ -1,4 +1,4 @@
 sudo apt install gcc -y
-gcc -o port_scanner_beta port_scanner_beta.c -lpthread
-sudo chmod +x port_scanner_beta
-sudo cp port_scanner_beta /usr/bin
+gcc -o portscan_beta portscan_beta.c -lpthread
+sudo chmod +x portscan_beta
+sudo cp portscan_beta /usr/bin
