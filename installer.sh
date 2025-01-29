f04d0399fcacfab32d76b5f55e5bf1789ec35d76
Frank E <131370932+byfranke@users.noreply.github.com>
2025-01-28 22:28:13 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,19 +11,6 @@ print_banner() {
     echo
 }
 
-print_banner
-
-echo "Choose an option below:"
-echo
-echo "1) Install Current Version"
-echo "2) Install Beta Version"
-echo "3) Install Old Version"
-echo "4) Check for Latest Version and Update"
-echo "5) Exit"
-echo
-
-read -p "Enter your choice: " choice
-
 install_portscan() {
     local source_file=$1
     local output_name="portscan"
@@ -47,75 +34,89 @@ install_portscan() {
 check_and_update() {
     echo "[*] Checking for the latest version on GitHub..."
     
-    # Habilita extglob para evitar erro de sintaxe
     shopt -s extglob
 
     GITHUB_REPO="https://github.com/byfranke/PortScan"
     TEMP_DIR=$(mktemp -d)
 
-    # Clone the latest version
     git clone "$GITHUB_REPO" "$TEMP_DIR"
 
-    # Move old files to obsolete, exceto installer.sh e obsolete folder
     mkdir -p obsolete
     find . -maxdepth 1 ! -name "installer.sh" ! -name "obsolete" -type f -exec mv {} obsolete/ \;
 
-    # Move new files to current directory
     mv "$TEMP_DIR"/* ./
     rm -rf "$TEMP_DIR"
 
     echo "[+] Updated to the latest version. Old files moved to 'obsolete'."
 }
 
-case $choice in
-    1)
-        if [ -f "portscan.c" ]; then
-            install_portscan "portscan.c"
-        else
-            echo "[!] portscan.c not found!"
-        fi
-        ;;
-    2)
-        if [ -f "portscan_beta.c" ]; then
-            install_portscan "portscan_beta.c"
-        else
-            echo "[!] portscan_beta.c not found!"
-        fi
-        ;;
-    3)
-        OLD_VERSIONS_DIR="Old Version"
-        if [ -d "$OLD_VERSIONS_DIR" ]; then
-            echo "Available old versions:"
-            files=("$OLD_VERSIONS_DIR"/*.c)
-            if [ ${#files[@]} -eq 0 ]; then
-                echo "[!] No old versions found."
-                exit 1
-            fi
+show_menu() {
+    while true; do
+        print_banner
+        echo "Choose an option below:"
+        echo
+        echo "1) Install Current Version"
+        echo "2) Install Beta Version"
+        echo "3) Install Old Version"
+        echo "4) Check for Latest Version and Update"
+        echo "5) Exit"
+        echo
+        read -p "Enter your choice: " choice
 
-            for i in "${!files[@]}"; do
-                echo "$((i+1))) ${files[$i]##*/}"
-            done
+        case $choice in
+            1)
+                if [ -f "portscan.c" ]; then
+                    install_portscan "portscan.c"
+                else
+                    echo "[!] portscan.c not found!"
+                fi
+                ;;
+            2)
+                if [ -f "portscan_beta.c" ]; then
+                    install_portscan "portscan_beta.c"
+                else
+                    echo "[!] portscan_beta.c not found!"
+                fi
+                ;;
+            3)
+                OLD_VERSIONS_DIR="Old Version"
+                if [ -d "$OLD_VERSIONS_DIR" ]; then
+                    echo "Available old versions:"
+                    files=("$OLD_VERSIONS_DIR"/*.c)
+                    if [ ${#files[@]} -eq 0 ]; then
+                        echo "[!] No old versions found."
+                        continue
+                    fi
 
-            read -p "Select a version to install (1-${#files[@]}): " version_choice
-            if [[ "$version_choice" =~ ^[0-9]+$ ]] && [ "$version_choice" -ge 1 ] && [ "$version_choice" -le "${#files[@]}" ]; then
-                selected_file="${files[$((version_choice-1))]}"
-                install_portscan "$selected_file"
-            else
-                echo "[!] Invalid selection!"
-            fi
-        else
-            echo "[!] Old Version folder not found!"
-        fi
-        ;;
-    4)
-        check_and_update
-        ;;
-    5)
-        echo "Exiting..."
-        exit 0
-        ;;
-    *)
-        echo "[!] Invalid option. Exiting..."
-        exit 1
-        ;;
-esac
+                    for i in "${!files[@]}"; do
+                        echo "$((i+1))) ${files[$i]##*/}"
+                    done
+
+                    read -p "Select a version to install (1-${#files[@]}): " version_choice
+                    if [[ "$version_choice" =~ ^[0-9]+$ ]] && [ "$version_choice" -ge 1 ] && [ "$version_choice" -le "${#files[@]}" ]; then
+                        selected_file="${files[$((version_choice-1))]}"
+                        install_portscan "$selected_file"
+                    else
+                        echo "[!] Invalid selection!"
+                    fi
+                else
+                    echo "[!] Old Version folder not found!"
+                fi
+                ;;
+            4)
+                check_and_update
+                ;;
+            5)
+                echo "Exiting..."
+                exit 0
+                ;;
+            *)
+                echo "[!] Invalid option. Please try again."
+                ;;
+        esac
+        echo
+        read -p "Press Enter to return to the menu..."
+    done
+}
+
+show_menu
