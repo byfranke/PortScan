0963998c92c127c410f180a6ec13c2ffb3fd1e13
Frank E <131370932+byfranke@users.noreply.github.com>
2025-01-28 21:21:24 -0300

    Update portscan_beta.c

--- portscan_beta.c
+++ portscan_beta.c
@@ -20,7 +20,7 @@ void print_banner() {
     printf("    |   |  _ \\    __|  __| \\___ \\    __|   _` |  __ \\ \n");
     printf("    ___/  (   |  |     |         |  (     (   |  |   | \n");
     printf("   _|    \\___/  _|    \\__| _____/  \\___| \\__,_| _|  _| \n");
-    printf("              github.com/byfranke Beta Version  \n");
+    printf("            github.com/byfranke Beta Version  \n");
     printf("\n");
 }
 
